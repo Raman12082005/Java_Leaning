64ba43aba8ca99c5d21068fb03b9cdabe250d8a5
Raman12082005 <rdeep1208y@gmail.com>
2026-01-04 19:42:08 +0530

    Add files via upload

---
+++ 06_more_of_datatypes.js
@@ -0,0 +1,22 @@
+/* JavaScript is a dynamicallt type language -> This means
+you do not need to explicity declare the data type of a 
+varible, and the type of a variable can change at runtime
+*/
+
+const hero = ["Ironman", "captain America", "Thor"]
+let myObj = {
+    name : "Manav",
+    age : 15
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof name);       // undefined
+console.log(typeof age);        // undefined
+console.log(typeof myObj);      // object
+console.log(typeof muFunction); // undefined
+console.log(typeof hero);       // object
+
+
